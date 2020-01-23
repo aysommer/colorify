05d6da1a4010488ee46b73b337605b94056f2711
Artyom <litovskys@gmail.com>
2020-01-24 00:02:22 +0300

    Updates

---
+++ precache-manifest.99f1f591136c2153e87fe8c1219b2b2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6e054efaab4b2d518a426bc8e456840",
+    "url": "/colorify/index.html"
+  },
+  {
+    "revision": "b74fbde1dc37b2c65b8a",
+    "url": "/colorify/static/css/main.a3e93a73.chunk.css"
+  },
+  {
+    "revision": "f1b7ef4ec25e5c766c67",
+    "url": "/colorify/static/js/2.b59ba0ef.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/colorify/static/js/2.b59ba0ef.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b74fbde1dc37b2c65b8a",
+    "url": "/colorify/static/js/main.d1d79ecd.chunk.js"
+  },
+  {
+    "revision": "3ee8edf9a8c630b9a659",
+    "url": "/colorify/static/js/runtime-main.bb9d75ff.js"
+  }
+]);
